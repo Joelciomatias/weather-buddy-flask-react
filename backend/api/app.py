0fa18b9440e77eb7fcfc3de6eaf41275424af9e7
joelcio <joelcio_psx@hotmail.com>
2021-02-13 16:06:10 -0300

    Weather api and caching

--- backend/api/app.py
+++ backend/api/app.py
@@ -1,8 +1,44 @@
-from flask import Flask
+from cacheout import Cache
+from flask import Flask, jsonify, request
+
 from api.integration import weather_api
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    res = weather_api.search_city('curitiba')
-    return res
+cache = Cache(maxsize=1000, ttl=5 * 60) # 5 minutes cache
+
+
+@app.route('/weather/<city_name>', methods=["GET"])
+def get_city_weather(city_name):
+
+    city_wealth = None
+
+    if city_name in cache:
+        city_wealth = cache.get(city_name)
+        print('used cache!')
+    else:
+        city_wealth = weather_api.search_city(city_name)
+        cache.set(city_name, city_wealth)
+
+    return city_wealth
+
+
+@app.route('/weather', methods=["GET"])
+def get_city_history():
+
+    result = {}
+    max_number = 5
+    if request.args.get('max'):
+        max_number = int(request.args.get('max'))
+
+    items = list(cache.items())  # get all items from cache
+
+    if len(items) and len(items) <= max_number:
+        result = items
+    elif len(items) > max_number:
+        inverted = list(reversed(items))
+        result = inverted[:max_number]  # last n items from cache
+
+    # items = dict(items)
+
+    return dict(result)
---
+++ backend/test.py
@@ -0,0 +1,22 @@
+# from pymemcache.client import base
+# client = base.Client(('localhost', 11211))
+# client.set('some_key', 'some value')
+
+# client.get('some_key')
+
+# rr = client.get('some_key')
+
+# print(rr)
+
+# from cacheout import Cache
+
+# cache = Cache()
+# cache.set('curitiba',{'d',3})
+# cache.set('curitib',{'d',3})
+# rr = cache.get('curitiba')
+
+# items = list(cache.items())
+
+# print(items.keys())
+
+
