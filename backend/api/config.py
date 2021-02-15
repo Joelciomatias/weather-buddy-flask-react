4315a2750c8da9632183f75433e71d179c33b737
joelcio <joelcio_psx@hotmail.com>
2021-02-15 20:07:11 -0300

    test

--- backend/api/config.py
+++ backend/api/config.py
@@ -1,5 +1,8 @@
 import os
 
-WEATHER_API_KEY = os.environ['WEATHER_API_KEY']
-WEATHER_API_URL = os.environ['WEATHER_API_URL']
+# WEATHER_API_KEY = os.environ['WEATHER_API_KEY']
+# WEATHER_API_URL = os.environ['WEATHER_API_URL']
 
+
+WEATHER_API_KEY='155a4457745da019871ac8d5348a9639'
+WEATHER_API_URL='https://api.openweathermap.org/data/2.5/weather'
